50c5d175c89169cde94383405330e9056d953039
kocyk908 <lukkoc01@gmail.com>
2025-02-16 15:00:58 +0100

    norminette

--- src/render_game.c
+++ src/render_game.c
@@ -6,75 +6,76 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 17:07:42 by lkoc              #+#    #+#             */
-/*   Updated: 2025/02/16 13:35:38 by marvin           ###   ########.fr       */
+/*   Updated: 2025/02/16 15:00:14 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-void    put_pixel(int x, int y, int color, t_game *game)
+void	put_pixel(int x, int y, int color, t_game *game)
 {
-    int index;
+	int		index;
 
-    if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
-        return ;
-    index = y * game->window.size_l + x * (game->window.bpp / 8);    
-    game->window.data[index] = color & 0xFF;
-    game->window.data[index + 1] = (color >> 8) & 0xFF;
-    game->window.data[index + 2] = (color >> 16) & 0xFF;
+	index = 0;
+	if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+		return ;
+	index = y * game->window.size_l + x * (game->window.bpp / 8);
+	game->window.data[index] = color & 0xFF;
+	game->window.data[index + 1] = (color >> 8) & 0xFF;
+	game->window.data[index + 2] = (color >> 16) & 0xFF;
 }
 
-void    draw_square(int x, int y, int size, int color, t_game *game)
+void	draw_square(int x, int y, int size, int color, t_game *game)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
 
-    i = 0;
-    while (i < size)
-    {
-        j = 0;
-        while (j < size)
-        {
-            put_pixel(x + i, y + j, color, game);
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			put_pixel(x + i, y + j, color, game);
+			j++;
+		}
+		i++;
+	}
 }
 
-void    clear_image(t_game *game)
+void	clear_image(t_game *game)
 {
-        int i;
-        int j;
+	int		i;
+	int		j;
 
-        i = 0;
-        while (i < WIDTH)
-        {
-            j = 0;
-            while (j < HEIGHT)
-            {
-                put_pixel(i, j, 0, game);
-                j++;
-            }
-            i++;
-        }
+	i = 0;
+	while (i < WIDTH)
+	{
+		j = 0;
+		while (j < HEIGHT)
+		{
+			put_pixel(i, j, 0, game);
+			j++;
+		}
+		i++;
+	}
 }
 
-void    draw_map(t_game *game)
+void	draw_map(t_game *game)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
 
-    i = 0;
-    while (i < game->map.height)
-    {
-        j = 0;
-        while (game->map.board_with_spaces[i][j])
-        {
-            if (game->map.board_with_spaces[i][j] == '1')
-                draw_square(j * 64, i * 64, 64, 0xFFFFFF, game);
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (i < game->map.height)
+	{
+		j = 0;
+		while (game->map.board_with_spaces[i][j])
+		{
+			if (game->map.board_with_spaces[i][j] == '1')
+				draw_square(j * 64, i * 64, 64, 0xFFFFFF, game);
+			j++;
+		}
+		i++;
+	}
 }
