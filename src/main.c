734c771c396bc8201c7411b6eb8abdbc56a09d9e
kocyk908 <lukkoc01@gmail.com>
2025-02-20 15:00:39 +0100

    main wyczyszczony + free

--- src/main.c
+++ src/main.c
@@ -70,77 +70,54 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
+void	rotate_player(t_player *player, double angle)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = player->dir_x;
+	old_plane_x = player->plane_x;
+	player->dir_x = player->dir_x * cos(angle) - player->dir_y * sin(angle);
+	player->dir_y = old_dir_x * sin(angle) + player->dir_y * cos(angle);
+	player->plane_x = player->plane_x * cos(angle)
+		- player->plane_y * sin(angle);
+	player->plane_y = old_plane_x * sin(angle) + player->plane_y * cos(angle);
+}
+
+void	move_in_direction(t_player *player, t_map *map,
+		double move_x, double move_y)
+{
+	if (map->board_with_spaces[(int)(player->y + move_y)]
+		[(int)(player->x + move_x)] != '1')
+	{
+		player->x += move_x;
+		player->y += move_y;
+	}
+}
+
 void	move_player(t_game *game)
 {
-	double		speed;
-	double		angle_speed;
-	double		new_x;
-	double		new_y;
+	double	speed;
+	double	angle_speed;
 
 	speed = SPEED;
 	angle_speed = 0.05;
-	new_x = 0;
-	new_y = 0;
 	if (game->player.right_rotate)
-	{
-		double old_dirx = game->player.dir_x;
-		game->player.dir_x = game->player.dir_x * cos(angle_speed) - game->player.dir_y * sin(angle_speed);
-		game->player.dir_y = old_dirx * sin(angle_speed) + game->player.dir_y * cos(angle_speed);
-
-		double old_planex = game->player.plane_x;
-		game->player.plane_x = game->player.plane_x * cos(angle_speed) - game->player.plane_y * sin(angle_speed);
-		game->player.plane_y = old_planex * sin(angle_speed) + game->player.plane_y * cos(angle_speed);
-	}
+		rotate_player(&game->player, angle_speed);
 	if (game->player.left_rotate)
-	{
-		double old_dirx = game->player.dir_x;
-		game->player.dir_x = game->player.dir_x * cos(-angle_speed) - game->player.dir_y * sin(-angle_speed);
-		game->player.dir_y = old_dirx * sin(-angle_speed) + game->player.dir_y * cos(-angle_speed);
-
-		double old_planex = game->player.plane_x;
-		game->player.plane_x = game->player.plane_x * cos(-angle_speed) - game->player.plane_y * sin(-angle_speed);
-		game->player.plane_y = old_planex * sin(-angle_speed) + game->player.plane_y * cos(-angle_speed);
-	}
+		rotate_player(&game->player, -angle_speed);
 	if (game->player.key_up)
-	{
-		new_x = game->player.x + game->player.dir_x * speed;
-		new_y = game->player.y + game->player.dir_y * speed;
-		if (game->map.board_with_spaces[(int)new_y][(int)new_x] != '1')
-		{
-			game->player.x = new_x;
-			game->player.y = new_y;
-		}
-	}
+		move_in_direction(&game->player, &game->map, game->player.dir_x
+			* speed, game->player.dir_y * speed);
 	if (game->player.key_down)
-	{
-		new_x = game->player.x - game->player.dir_x * speed;
-		new_y = game->player.y - game->player.dir_y * speed;
-		if (game->map.board_with_spaces[(int)new_y][(int)new_x] != '1')
-		{
-			game->player.x = new_x;
-			game->player.y = new_y;
-		}
-	}
+		move_in_direction(&game->player, &game->map, -game->player.dir_x
+			* speed, -game->player.dir_y * speed);
 	if (game->player.key_left)
-	{
-		new_x = game->player.x - game->player.plane_x * speed;
-		new_y = game->player.y - game->player.plane_y * speed;
-		if (game->map.board_with_spaces[(int)new_y][(int)new_x] != '1')
-		{
-			game->player.x = new_x;
-			game->player.y = new_y;
-		}
-	}
+		move_in_direction(&game->player, &game->map, -game->player.plane_x
+			* speed, -game->player.plane_y * speed);
 	if (game->player.key_right)
-	{
-		new_x = game->player.x + game->player.plane_x * speed;
-		new_y = game->player.y + game->player.plane_y * speed;
-		if (game->map.board_with_spaces[(int)new_y][(int)new_x] != '1')
-		{
-			game->player.x = new_x;
-			game->player.y = new_y;
-		}
-	}
+		move_in_direction(&game->player, &game->map, game->player.plane_x
+			* speed, game->player.plane_y * speed);
 }
 
 void	set_ray_direction(t_ray *ray, t_player *player, double camera_x)
